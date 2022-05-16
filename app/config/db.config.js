64a814798d3ebe16c6b66238a3dcac1a73d17441
rescarabelo <rafael@scenario.ind.br>
2022-05-16 15:39:46 -0300

    mongo url change

--- app/config/db.config.js
+++ app/config/db.config.js
@@ -1,3 +1,3 @@
 module.exports = {
-    url: "mongodb://rafael:rafael123@docdb-2022-05-16-15-59-18.cluster-c1tdwqk30pic.us-east-1.docdb.amazonaws.com:27017/?tls=true&tlsCAFile=rds-combined-ca-bundle.pem&replicaSet=rs0&readPreference=secondaryPreferred&retryWrites=false"
+    url: "mongodb+srv://rafael:rafael123@ead2-tc2.l7aiz.mongodb.net/?retryWrites=true&w=majority"
 };
