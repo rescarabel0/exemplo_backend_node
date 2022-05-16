b377f57dbebf2523586b511203bbfcf5783ab4c3
rescarabelo <rafael@scenario.ind.br>
2022-05-16 15:23:18 -0300

    adapting to deploy

--- app/config/db.config.js
+++ app/config/db.config.js
@@ -1,3 +1,3 @@
 module.exports = {
-    url: "mongodb://localhost:27017/bancoTeste"
+    url: "mongodb://rafael:rafael123@docdb-2022-05-16-15-59-18.cluster-c1tdwqk30pic.us-east-1.docdb.amazonaws.com:27017/?tls=true&tlsCAFile=rds-combined-ca-bundle.pem&replicaSet=rs0&readPreference=secondaryPreferred&retryWrites=false"
 };
